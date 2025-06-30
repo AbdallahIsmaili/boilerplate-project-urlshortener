754303179167fce7ae6c5b6359dfa13a46be2268
AbdallahIsmaili <89218851+AbdallahIsmaili@users.noreply.github.com>
2025-06-30 19:31:14 +0100

    Add URL shortener API endpoints
    Implemented POST /api/shorturl to create short URLs with validation and DNS lookup, and GET /api/shorturl/:short_url to redirect to the original URL. Also updated the start script to use nodemon for development.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +23,64 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+let urlAndShorts = []; 
+
+app.post("/api/shorturl", (req, res) => {
+  const { url } = req.body; 
+  
+  try {
+    const baseUrl = new URL(url);
+
+    if (!['http:', 'https:'].includes(baseUrl.protocol)) {
+      return res.json({ error: 'invalid url' });
+    }
+  } catch (err) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const { hostname } = new URL(url);
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    } else {
+      const urltoLower = url.toLowerCase();
+      const isUrlAlreadyExisted = urlAndShorts.find(item => 
+        item.original_url.toLowerCase() === urltoLower
+      );
+
+      if(isUrlAlreadyExisted) {
+        return res.json({
+          original_url: isUrlAlreadyExisted.original_url,
+          short_url: isUrlAlreadyExisted.short_url
+        });
+      } else {
+        const short_url = urlAndShorts.length + 1; 
+        const newShortUrl = { original_url: url, short_url };
+        urlAndShorts.push(newShortUrl);
+        return res.json(newShortUrl);
+      }
+    }
+  });
+});
+
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+
+  const short_url = parseInt(req.params.short_url); 
+
+  const isExist = urlAndShorts.find(item => item.short_url === short_url); 
+
+  if (isExist) {
+    return res.redirect(isExist.original_url); 
+
+  } else {
+    return res.json({ error: 'invalid url' }); 
+  }
+  
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
